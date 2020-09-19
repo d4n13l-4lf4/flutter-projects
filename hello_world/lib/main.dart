0c64bd8ec099e94d6e2cbbcf5007e2e40dcaf4f9
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-09-18 23:45:28 -0500

    SingleChildScrollView + Padding

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main () => runApp(MyApp());
 
@@ -7,13 +9,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello World Travel Title",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Hello World Travel App")),
-        body: Center(
-          child: Text("Hello World Travel")
+        title: "Hello World Travel Title",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Hello World Travel App"),
+              backgroundColor: Colors.deepPurple,
+            ),
+            body: Builder(builder: (context) => SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                        child: Column(children: [
+                          Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text("Hello World Travel",
+                                style: TextStyle(
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue[800]
+                                ),
+                              )
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                'Discover the World',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.deepPurpleAccent
+                                ),
+                              )
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Image.network(
+                                "https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg",
+                                height: 350,
+                              )
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(15),
+                              child: RaisedButton(
+                                child: Text('Contact Us'),
+                                onPressed: () => contactUs(context),
+                              )
+                          )
+                        ],)
+                    )
+                )
+            )
+            )
         )
-      )
     );
   }
+}
+
+void contactUs(BuildContext context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            title: Text('Contact Us'),
+            content: Text("Mail us at hello@world.com"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Close'),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ]
+        );
+      }
+  );
 }
